8e3b1356b8c444a47eb0f508ad6449c2229fa0f6
froeset <168585726+froeset@users.noreply.github.com>
2026-04-03 13:54:53 -0400

    add load with tour files

--- load_experiments.py
+++ load_experiments.py
@@ -9,6 +9,16 @@ def return_experiments():
             experiments.append((tsp_file, tour_file))
     return experiments
 
+def return_experiments_with_tours():
+    experiments = []
+    for file in os.listdir("res"):
+        if file.endswith(".tsp"):
+            tsp_file = os.path.join("res", file)
+            tour_file = os.path.join("res", file.replace(".tsp", ".opt.tour"))
+            if os.path.exists(tour_file):
+                experiments.append((tsp_file, tour_file))
+    return experiments
+
 def print_experiments(experiments):
     for tsp_file, tour_file in experiments:
         print(f"TSP file: {tsp_file}, Tour file: {tour_file}")
